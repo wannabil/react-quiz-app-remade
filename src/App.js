206a227eaf92af28a27f7d18a2daaeb33141e9ba
Wan Nabil <wannabil.xyz@gmail.com>
2023-05-26 15:46:55 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { listOfQuestion } from './Quiz.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>You will build a simple React quiz app where users can click on option buttons to answer questions,
+           next question rendered each time option is selected and at the end, the final score is displayed.</p>
+        <QuizApp />
     </div>
   );
 }
 
 export default App;
+
+
+
+export default function QuizApp() {
+  const [currentQuestion, setcurrentQuestion] = useState(0);
+  const [totalPoint, settotalPoint] = useState(0);
+  const [finalResult, setfinalResult] = useState(false);
+  
+
+  function handleNextClick() {
+    setindex(index + 1);
+  }
+
+  let quiz = currentQuestion[index];
+  return (
+    <>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {currentQuestion.length})
+      </h3>
+    </>
+  );
+}
---
+++ src/Quiz.js
@@ -0,0 +1,6 @@
+export const listOfQuestion = [
+  { 
+    question: 'Which of the following brand is NOT sneaker brand?',
+    choices: ['Nike(isFalse)', 'Adidas(isFalse)', 'Ikea(isTrue)', 'Converse(isFalse)'],,
+  },
+];
